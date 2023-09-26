21251cd9cc7e3c1c1ed5d557fa97e70b8aad42e1
dsmith111 <davidcsmith12@gmail.com>
2023-09-26 23:21:42 +0000

    Fix spacing

--- test/plugin/test_filter_process_redfishalert.rb
+++ test/plugin/test_filter_process_redfishalert.rb
@@ -20,19 +20,19 @@ class ProcessRedfishAlertFilterTest < Test::Unit::TestCase
   SUPERMICRO_CONFIG = CONFIG.gsub(/hardware SDFLEX/, 'hardware SUPERMICRO')
 
   def create_driver(conf)
-          Fluent::Test::Driver::Filter.new(Fluent::ProcessRedfishAlert) do
+        Fluent::Test::Driver::Filter.new(Fluent::ProcessRedfishAlert) do
             # for testing
             def getPassword()
-                      return 'testPassword'
+              return 'testPassword'
             end
 
             def callTestRedfishGetAPI(host, resourceURI)
-              res = '{
-                  "SerialNumber":"123456",
-                  "PowerState":"On"
-              }'
+                res = '{
+                    "SerialNumber":"123456",
+                    "PowerState":"On"
+                }'
 
-                      return JSON.parse(res)
+                return JSON.parse(res)
             end
 
             def getRackGroupIdentifier(host)
@@ -56,42 +56,42 @@ class ProcessRedfishAlertFilterTest < Test::Unit::TestCase
     d = create_driver(CONFIG)
     assert_equal 'testcolo', d.instance.coloregion
     assert_equal 'testuser', d.instance.username
-  end
+end
 
   def filter(records, conf = CONFIG)
-      d = create_driver(conf)
-      d.run(default_tag: "redfish.alert") do
-        records.each do |record|
-          d.feed(record)
-        end
-      end
-      d.filtered_records
-  end
-  
+    d = create_driver(conf)
+    d.run(default_tag: "redfish.alert") do
+       records.each do |record|
+           d.feed(record)
+       end
+    end
+    d.filtered_records
+end
+
   def test_redfish_filter
     records = [
         # A Sample Redfish Event
         {
             "Name":"Events",
             "Events":[
-                {
-                    "EventTimestamp":"2020-07-08T21:27:34Z",
+              {
+                  "EventTimestamp":"2020-07-08T21:27:34Z",
                   "MessageArgs":[
                      "rack1/chassis_u24/psu3"
                   ],
-                    "Severity":"OK",
-                    "Message":"Test Message for rack1/chassis_u24/psu3",
-                    "MemberId":"1",
-                    "MessageId":"MessageId",
-                    "OriginOfCondition":"/URI/to/resource",
-                    "EventId":"EventID",
-                    "Oem":{},
-                    "EventType":"Alert"
+                  "Severity":"OK",
+                  "Message":"Test Message for rack1/chassis_u24/psu3",
+                  "MemberId":"1",
+                  "MessageId":"MessageId",
+                  "OriginOfCondition":"/URI/to/resource",
+                  "EventId":"EventID",
+                  "Oem":{},
+                  "EventType":"Alert"
                 }
             ],
             "@odata.type":"#Event.1.1.2.Event",
             "REMOTE_ADDR":"1.2.3.4"
-        }
+          }
     ]
     filtered_records = filter(records)
     assert_equal(records[0].length, filtered_records[0].length, "Incorrect record size for SDFLEX")
